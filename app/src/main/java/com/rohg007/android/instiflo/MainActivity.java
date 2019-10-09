69baee1e6542780563ec1a63268a897532029891
gupta <17dcs012@lnmiit.ac.in>
2019-10-09 19:09:56 +0530

    Added Login Fragment

--- app/src/main/java/com/rohg007/android/instiflo/MainActivity.java
+++ app/src/main/java/com/rohg007/android/instiflo/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.rohg007.android.instiflo.ui.LoginFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container,new LoginFragment()).commit();
     }
 }
