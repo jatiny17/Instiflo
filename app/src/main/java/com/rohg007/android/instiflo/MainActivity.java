3300e416765af78c644a32699b1db7ea0a3ccbe2
gupta <17dcs012@lnmiit.ac.in>
2019-10-16 03:29:51 +0530

    navigation drawer is operational, bottom navigation bar added

--- app/src/main/java/com/rohg007/android/instiflo/MainActivity.java
+++ app/src/main/java/com/rohg007/android/instiflo/MainActivity.java
@@ -1,9 +1,15 @@
 package com.rohg007.android.instiflo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,31 +18,41 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.rohg007.android.instiflo.ui.LoginActivity;
 import com.rohg007.android.instiflo.ui.LoginFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     String email;
     String name;
     private static boolean FLAG= false;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.logout);
+        Toolbar toolbar = findViewById(R.id.toobar_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.
-                Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
-                requestEmail().
-                build();
 
-        final GoogleSignInClient googleSignInClient= GoogleSignIn.getClient(this,gso);
-        final GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
 
         if(user!=null){
             email=user.getEmail();
@@ -47,17 +63,64 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Toast.makeText(getApplicationContext(),"Logged in as"+email+" "+name,Toast.LENGTH_SHORT).show();
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(account==null) {
-                    FirebaseAuth.getInstance().signOut();
-                } else {
-                    googleSignInClient.signOut();
-                }
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.menu_my_events:
+                Toast.makeText(this,"My Events Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_my_purchases:
+                Toast.makeText(this,"My Purchases Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_my_products:
+                Toast.makeText(this,"My Products Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_settings:
+                Toast.makeText(this,"Settings Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_about:
+                Toast.makeText(this,"About Us Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                logOut();
+        }
+
+        return true;
+    }
+
+    private void logOut(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.
+                Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
+                requestEmail().
+                build();
+
+        GoogleSignInClient googleSignInClient= GoogleSignIn.getClient(this,gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if(account==null) {
+            FirebaseAuth.getInstance().signOut();
+        } else {
+            googleSignInClient.signOut();
+        }
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
     }
 }
